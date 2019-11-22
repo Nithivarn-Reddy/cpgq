f404878b426c6abf312f92571ae0fcd93b74fcef
nithivarn_reddy <nithivarn.reddy.shanigaram-1@ou.edu>
2019-11-22 12:02:02 -0600

    changes made to tasks

--- cpgq/tasks/tasks.py
+++ cpgq/tasks/tasks.py
@@ -18,3 +18,21 @@ def test_grouping(x,y):
     chain = (add.s(x,y) | group(double.s(), triple.s()))
     chain.delay()
     return "Kicked Off tasks"
+
+contacts=[]
+
+@task
+def user_create(username,first,last,email):
+    
+    return [{ 'username' : username , 'first' : first ,'last':last ,'email':email }]
+
+@task
+def import_contacts(l):
+    global contacts.append(l)
+    return '\t new user added ' + l[-1].username + 'length of contacts ' +len(contacts)
+
+@task
+def insert_user(username,first,last,email):
+    result = (user_create.s(username,first,last,email) | import_contacts.s())
+    return result.get()
+

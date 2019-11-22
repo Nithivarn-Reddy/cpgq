74f5cc1dc0cfe9638d74e8100e759390bd2f087c
nithivarn_reddy <nithivarn.reddy.shanigaram-1@ou.edu>
2019-11-22 11:50:31 -0600

    usercreatetask created

---
+++ cpgq/tasks/usercreationtask.py
@@ -0,0 +1,17 @@
+from celery.task import task
+from celery import signature, group
+contacts=[]
+@task
+def user_create(username,first,last,email):
+    
+    return [{ 'username' : username , 'first' : first ,'last':last ,'email':email }]
+
+@task
+def import_contacts(l):
+    global contacts.append(l)
+    return '\t new user added ' + l[-1].username + 'length of contacts ' +len(contacts)
+
+@task
+def insert_user(username,first,last,email):
+    result = (user_create.s(username,first,last,email) | import_contacts.s())
+    return result.get()
